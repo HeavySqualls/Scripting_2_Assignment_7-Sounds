0f44fe035ad7304ac3489b97c610c552946db31c
SquallzFunk <39074621+SquallzFunk@users.noreply.github.com>
2019-04-16 19:55:51 -0400

    Built the repos

---
+++ Scripting_2_Assignment_7-Sounds/Assets/SoundsScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundsScript : MonoBehaviour
+{
+    AudioSource[] AvailableAudioSources;
+    AudioSource[] NowPlayingAudioSources;
+
+    void Start()
+    {
+        AvailableAudioSources = GetComponents<AudioSource>();
+        NowPlayingAudioSources = new AudioSource[AvailableAudioSources.Length];
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            
+        }
+    }
+}
