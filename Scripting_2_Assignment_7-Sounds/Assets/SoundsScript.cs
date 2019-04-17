7d58bcb13cdbe930700ac62b77b0d82497d20e05
SquallzFunk <39074621+SquallzFunk@users.noreply.github.com>
2019-04-16 20:29:29 -0400

    Added more stuff

--- Scripting_2_Assignment_7-Sounds/Assets/SoundsScript.cs
+++ Scripting_2_Assignment_7-Sounds/Assets/SoundsScript.cs
@@ -4,19 +4,41 @@ using UnityEngine;
 
 public class SoundsScript : MonoBehaviour
 {
-    AudioSource[] AvailableAudioSources;
-    AudioSource[] NowPlayingAudioSources;
+    AudioSource[] audioSources;
 
     void Start()
     {
-        AvailableAudioSources = GetComponents<AudioSource>();
-        NowPlayingAudioSources = new AudioSource[AvailableAudioSources.Length];
+        audioSources = GetComponents<AudioSource>();
     }
+
     void Update()
     {
+        AudioSource winner = null;
+
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            
+            float winnerRatio = 0;
+            for (int i = 0; i < audioSources.Length; i++)
+            {
+                if (audioSources[i].isPlaying == false)
+                {
+                    winner = audioSources[i];
+                    break;
+                }
+                else
+                {
+                    float currentSoundCompletionRatio = audioSources[i].time / audioSources[i].clip.length;
+                    if (currentSoundCompletionRatio > winnerRatio)
+                    {
+                        winner = audioSources[i];
+                        winnerRatio = currentSoundCompletionRatio;
+                    }
+                }
+            }
+            if (winner)
+            {
+                winner.Play();
+            }
         }
     }
 }
